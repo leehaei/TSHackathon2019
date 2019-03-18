de984c8baacb2d3ecc00c4ea32dd615e72895e9c
Haeim Lee <h283lee@uwaterloo.ca>
2019-03-18 09:53:05 -0400

    started chatbot

--- app/src/main/java/com/example/haeim/tshackathon2019/BotActivity.java
+++ app/src/main/java/com/example/haeim/tshackathon2019/BotActivity.java
@@ -7,64 +7,48 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import com.github.bassaer.chatmessageview.model.ChatUser;
-import com.github.bassaer.chatmessageview.model.Message;
-import com.github.kittinunf.fuel.Fuel;
-//import com.github.kittinunf.fuel.android.extension.responseJson;
-import com.github.kittinunf.fuel.core.FuelManager;
 
-import ai.api.AIDataService;
-import ai.api.AIServiceContextBuilder;
-import ai.api.android.AIConfiguration;
-import ai.api.model.AIRequest;
-
-import ai.api.AIServiceContext;
-import ai.api.AIServiceContextBuilder;
-import ai.api.android.AIConfiguration;
-import ai.api.android.AIDataService;
+import ai.api.android.AIService;
 import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
-
-import java.util.UUID;
+import ai.api.model.AIError;
+import ai.api.android.AIConfiguration;
 
 
 public class BotActivity extends AppCompatActivity {
 
-    //String ACCESS_TOKEN = "f8ad05838e684915993f0f281ae0be85";
+    public interface AIListener {
+        void onResult(AIResponse result); // here process response
+        void onError(AIError error); // here process error
+        void onAudioLevel(float level); // callback for sound level visualization
+        void onListeningStarted(); // indicate start listening here
+        void onListeningCanceled(); // indicate stop listening here
+        void onListeningFinished(); // indicate stop listening here
+    }
 
-    // Android client
-    private AIRequest aiRequest;
-    private AIDataService aiDataService;
-    private AIServiceContext customAIServiceContext;
-    private String uuid = UUID.randomUUID().toString();
-
-    // Java V2
-//    private SessionsClient sessionsClient;
-//    private SessionName session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bot);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        FuelManager.instance.baseHeaders = mapOf(
-//                "Authorization" to "Bearer $ACCESS_TOKEN"
-//        )
 
-
-    }
-
-    private void initChatbot() {
+        //Instance of AIConfiguration, specifying the access token, locale, and recognition engine.
         final AIConfiguration config = new AIConfiguration("f8ad05838e684915993f0f281ae0be85",
                 AIConfiguration.SupportedLanguages.English,
                 AIConfiguration.RecognitionEngine.System);
-        aiDataService = new AIDataService(this, config);
-        customAIServiceContext = AIServiceContextBuilder.buildFromSessionId(uuid);
-        aiRequest = new AIRequest();
+
+        //Use the AIConfiguration object to get a reference to the AIService, which will make the query requests.
+        AIService aiService = AIService.getService(this, config);
+
+        //Set the AIListener instance for the AIService instance.
+        aiService.startListening();
+
+        String msg = "hi";
+        AIRequest aiRequest = new AIRequest();
+        aiRequest.setQuery(msg);
+
+        //String botReply = aiResponse.getResult().getFulfillment().getSpeech();
+
     }
-
-
-
 
 }
